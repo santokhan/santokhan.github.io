beeda3fb3cf02a0bd2d4293ee5110bf08942b207
Santo Khan <santok54@gmail.com>
2021-04-23 09:16:27 +0600

    Update navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -9,11 +9,8 @@ class Navbar {
 
     }
 }
-
 function navbarListeners() {
     const ObjNav = new Navbar();
     const bTn = { navHideBtn: document.getElementById("navHideBtn"), navShowBtn: document.getElementById("navHideBtn") }
 }
 document.addEventListener("DOMContectLoaded", () => { navbarListeners(); });
-console.log("I have create a json file.");
-console.log("I have create a json file.2");
