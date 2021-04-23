2a3cea9c761b0ced78b1f45cae5e80dce32aa934
Santo Khan <santok54@gmail.com>
2021-04-23 14:50:45 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 'user strict';
 class Portfolio {
     constructor() {
-        this.homePage = document.getElementById("homePage");
-        this.profilePage = document.getElementById("profilePage");
-        this.resumePage = document.getElementById("resumePage");
-        this.infoPage = document.getElementById("infoPage");
-        this.contactPage = document.getElementById("contactPage");
+            this.profileCart = document.getElementById("profileCart");
+            this.homePage = document.getElementById("homePage");
+            this.profilePage = document.getElementById("profilePage");
+            this.resumePage = document.getElementById("resumePage");
+            this.infoPage = document.getElementById("infoPage");
+            this.contactPage = document.getElementById("contactPage");
 
-        this.typoAnimate = document.getElementById("typoAnimate");
-        //console.log(this);
-    }
-    /* leftNavbarShowHide() {
-        if (this.hiddenLeftNavbar.style.display === "none") {
-            this.hiddenLeftNavbar.style.display = 'block';
-        } else {
-            this.hiddenLeftNavbar.style.display = 'none';
+            this.typoAnimate = document.getElementById("typoAnimate");
+            //console.log(this);
         }
-    } */
+        /* leftNavbarShowHide() {
+            if (this.hiddenLeftNavbar.style.display === "none") {
+                this.hiddenLeftNavbar.style.display = 'block';
+            } else {
+                this.hiddenLeftNavbar.style.display = 'none';
+            }
+        } */
     ShowTab(props) {
         props.classList.replace("d-none", "d-block");
     }
     HideTab(props) {
         props.classList.replace("d-block", "d-none");
+    }
+    ShowNavbar() {
+        if (this.profileCart.style.display === "none") {
+            this.profileCart.style.display = "block";
+        } else {
+            this.profileCart.style.display = "none";
+        }
     }
     ShowHomePage() {
         this.ShowTab(this.homePage);
@@ -77,23 +85,47 @@ class Portfolio {
         }
     }
 }
+
 function addListeners() {
     const portFolioObj = new Portfolio();
     const bTn = {
+        menuBarBtn: document.getElementById("menuIcon");
         homeBtn: document.getElementById("homePageBtn"),
         profileBtn: document.getElementById("profileBtn"),
         resumeBtn: document.getElementById("resumeBtn"),
         infoBtn: document.getElementById("infoBtn"),
         contactBtn: document.getElementById("contactPageBtn")
     }
-    bTn.homeBtn.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.ShowHomePage(); });
-    bTn.profileBtn.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.ShowProfilePage(); });
-    bTn.resumeBtn.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.ShowResumePage(); });
-    bTn.infoBtn.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.ShowInfoPage(); });
-    bTn.contactBtn.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.ShowContactPage(); });
+    bTn.menuBarBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowNavbar();
+    })
+    bTn.homeBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowHomePage();
+    });
+    bTn.profileBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowProfilePage();
+    });
+    bTn.resumeBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowResumePage();
+    });
+    bTn.infoBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowInfoPage();
+    });
+    bTn.contactBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.ShowContactPage();
+    });
 
     const typoClick = document.getElementById("typoAnimate");
-    typoClick.addEventListener("click", (e) => { e.preventDefault(); portFolioObj.StopTypo(); });
+    typoClick.addEventListener("click", (e) => {
+        e.preventDefault();
+        portFolioObj.StopTypo();
+    });
 
 }
 class DirectScript extends Portfolio {
@@ -110,4 +142,3 @@ document.addEventListener("DOMContentLoaded", () => {
     const DirScriptObj = new DirectScript();
     DirScriptObj.AutoHomePage(); //call direct srcipt on load
 });
-
