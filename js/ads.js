8279af828fffe9df96f2d3fb980d6dac8c5682bd
Santo Khan <santok54@gmail.com>
2021-07-15 21:59:09 +0600

    Blank

--- js/ads.js
+++
@@ -1,4 +0,0 @@
-const adsTimes = document.getElementById("adsTimes");
-const ads = document.getElementById("ads");
-
-adsTimes.addEventListener("click", (e) => { e.preventDefault(); console.log(adsTimes); ads.classList.add("d-none"); });
