a5f7571601f5d681f73864fb5942084a58225f1b
Santo Khan <santok54@gmail.com>
2021-08-15 18:14:28 +0600

    Script Update

--- js/app.js
+++ js/app.js
@@ -1,27 +1,69 @@
 'use strict';
-(function () {
+{
+    /** Most important part */
     const toggler = document.querySelector("#themeToggler");
-    const icon = document.querySelector("#themeToggler>#icon");
+    //const icon = document.querySelector("#themeToggler>#icon");
     let onclick = false;
 
-    function toggleFunc() {
-        if (!onclick) {
-            /** icon.classList.replace("fa-sun", "fa-moon"); */
-            toggler.innerHTML = `<i class="fas fa-moon"></i>`;
-            document.body.setAttribute("data-theme", "night");
-            onclick = true;
-        } else {
-            // icon.classList.replace("fa-moon", "fa-sun");
+    // localStorage getitem and check day/night status
+    function checkDataTheme() {
+        let dataThemeMode = localStorage.getItem("data-theme");
+        document.body.setAttribute("data-theme", dataThemeMode);
+        if (dataThemeMode === "day") {
             toggler.innerHTML = `<i class="fas fa-sun"></i>`;
-            document.body.setAttribute("data-theme", "day");
+        } else if (dataThemeMode === "night") {
+            toggler.innerHTML = `<i class="fas fa-moon"></i>`;
+            // when data theme night
+            onclick = true;
+        }
+    }
+    checkDataTheme();
+
+    // locastorage setitem ontoggle theme
+    function toggleFunc() {
+        if (onclick === false) {
+            localStorage.setItem("data-theme", "night");
+            onclick = true;
+        } else if (onclick === true) {
+            localStorage.setItem("data-theme", "day");
             onclick = false;
         }
-        console.log(icon);
+
+        // call after if else check
+        checkDataTheme();
     }
-
     toggler.addEventListener("click", toggleFunc);
-})();
+}
 
+
+/** copy of top */
+{
+    function themeToggler() {
+        const toggler = document.querySelector("#themeToggler");
+        const icon = document.querySelector("#themeToggler>#icon");
+        let onclick = false;
+
+        function toggleFunc() {
+            if (!onclick) {
+                /** icon.classList.replace("fa-sun", "fa-moon"); */
+                toggler.innerHTML = `<i class="fas fa-moon"></i>`;
+                document.body.setAttribute("data-theme", "night");
+                onclick = true;
+            } else {
+                // icon.classList.replace("fa-moon", "fa-sun");
+                toggler.innerHTML = `<i class="fas fa-sun"></i>`;
+                document.body.setAttribute("data-theme", "day");
+                onclick = false;
+            }
+            //console.log(icon);
+        }
+        //toggler.addEventListener("click", toggleFunc);
+    }
+    //themeToggler();
+}
+
+
+/** Most important part */
 (function () {
     const loaderBox = document.querySelector(".loader__box");
     setTimeout(() => {
